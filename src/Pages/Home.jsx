fa45194cc3907ccbf29721cab0dfad6b9004309a
Jordan W <famdalorian@proton.me>
2023-06-20 10:31:03 +0100

    her section updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,9 +22,9 @@ function Home() {
       <div className="herosectionRightContainer">
         <HerosectionRight
           image={require("../assets/bus1.png")}
-          title="Welcome to XXISP.eth"
-          subheading="Center of Excellence"
-          paragraph="At XXISP, we believe that true progress begins with disruptive thinking and relentless determination. Our founder, XXISP, has a proven track record of pushing boundaries and challenging the status quo. With a deep-rooted commitment to making a positive impact, XXISP has set out on an extraordinary mission to transform industries and create a better world for all."
+          title="Be the best you!"
+          subheading="Mentality Forge"
+          paragraph="At XXISP, we embrace the power of bold ideas and unwavering resolve as catalysts for genuine advancement. Our visionary leader, XXISP, has consistently shattered conventional norms and redefined possibilities. With an unwavering dedication to driving positive change, XXISP embarks on an extraordinary journey to revolutionize industries and forge a brighter future for everyone."
         />
       </div>
     </div>
